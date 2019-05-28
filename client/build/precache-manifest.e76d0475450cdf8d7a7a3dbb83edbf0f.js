eb1f759932523978683ff36dadb415a88bf2fd5e
TsvAliaksandra <tsvirko8@gmail.com>
2019-05-28 16:14:44 +0300

    setup app

---
+++ client/build/precache-manifest.e76d0475450cdf8d7a7a3dbb83edbf0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "20e8666bac248e1a15c3",
+    "url": "/static/css/main.a7577d56.chunk.css"
+  },
+  {
+    "revision": "20e8666bac248e1a15c3",
+    "url": "/static/js/main.20e8666b.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ec060e6ada70efcf1207a5f1552481f5",
+    "url": "/index.html"
+  }
+];
---
+++ server/API/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const app = express();
+const port = process.env.PORT || 3000;
+
+const STATIC_PATH = path.join(__dirname, '..', '..', 'client', 'build');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(STATIC_PATH));
+app.set('views', STATIC_PATH);
+
+app.get("*", (req, res) => {
+  res.sendFile(`${STATIC_PATH}/index.html`);
+});
+
+app.listen(port, () => console.log("listening on port", port));
