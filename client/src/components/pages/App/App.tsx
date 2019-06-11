1648a69fbc59394b57da57bf0ad8d59684b82263
TsvAliaksandra <tsvirko8@gmail.com>
2019-06-11 16:45:43 +0300

    added typescript to the project

--- client/src/components/pages/App/App.tsx
+++ client/src/components/pages/App/App.tsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
-class App extends Component {
+interface IProps {}
+
+class App extends Component <IProps> {
   render() {
     console.log(process.env);
     return (
